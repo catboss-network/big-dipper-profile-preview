f0eda509dd5f45621f1995f4487c87da4f0cf18f
Wingman <ryuash123@gmail.com>
2020-09-09 16:13:22 +0800

    initial setup

---
+++ src/screens/home/index.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { useTranslation } from 'i18n';
+import {
+  Example, Layout,
+} from '@components';
+import Button from '@material-ui/core/Button';
+import { useHomeHook } from './hooks';
+
+const Home = () => {
+  const { t } = useTranslation('common');
+  const {
+    data,
+    loading,
+    error,
+  } = useHomeHook();
+
+  if (error) {
+    console.log('there is an error');
+  }
+
+  return (
+    <Layout>
+      <Button
+        variant="contained"
+        color="primary"
+      >
+        Hello World
+      </Button>
+      <h1>{t('bigDipper')}</h1>
+      <h1>{t('forbole')}</h1>
+      <p>
+        home page on port
+      </p>
+      {!loading && <h3>done loading!</h3>}
+      {!loading && !error && data.rates.slice(0, 7).map((x) => (
+        <p
+          key={x.currency}
+        >
+          {x.currency}
+        </p>
+      ))}
+      <Example />
+    </Layout>
+  );
+};
+
+export default Home;
