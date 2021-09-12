bcad73570edb667cfadca8381424f20b6f6a66c2
Wingman <ryuash123@gmail.com>
2021-09-12 15:48:19 +0800

    test

--- next.config.js
+++ next.config.js
@@ -1,19 +1,3 @@
-// const isProd = process.env.NODE_ENV === 'production';
-
-// module.exports = nextTranslate({
-//   assetPrefix: isProd ? '/big-dipper-profile-preview/' : '',
-//   webpack: (config) => {
-//     config.module.rules.push({
-//       test: /\.svg$/,
-//       use: ['@svgr/webpack'],
-//     });
-//     return config;
-//   },
-// });
-
-const isProd = process.env.NODE_ENV === 'production';
-
 module.exports = {
-  // Use the prefix in production and not development.
-  assetPrefix: isProd ? '/big-dipper-profile-preview/' : '',
+  distDir: 'build',
 };
