0880a2b4ecc6017c67b201f1ed1ebd952cfecd63
ryuash <ryuash@users.noreply.github.com>
2021-09-12 07:49:31 +0000

    deploy: bcad73570edb667cfadca8381424f20b6f6a66c2

---
+++ static/nCdARGbYRYDfA-i4PxhiV/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-b796ba5b8cc170190312.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-3da6e4cad07a3763ab08.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
